405c307b0b2aef8db8fcb889f65b91e5eaa3577a
SatyamMishra49 <skm28112001@gmail.com>
2025-04-17 01:35:15 +0530

    added pages

--- food-app/src/app/recipe-list/page.js
+++ food-app/src/app/recipe-list/page.js
@@ -1,5 +1,21 @@
+import RecipeLists from "@/components/recipe-list/page";
 
-export default function RecipeList() {
+async function FetchListOfRecipe(){
+    try {
+        const apiResponse = fetch('https://dummyjson.com/recipes');
+        const data = (await apiResponse).json();
 
-    return 
+        return data?.recipes
+    } catch (e) {
+        throw new Error(e)
+    }
+}
+
+export default async function RecipeList() {
+
+    const recipeList = await FetchListOfRecipe();
+
+    return <div>
+        <RecipeLists />
+    </div>
 }
---
+++ food-app/src/components/recipe-list/page.js
@@ -0,0 +1,6 @@
+
+export default function RecipeLists(){
+    return <div>
+        <h1>Data from RecipeList</h1>
+    </div>
+}
